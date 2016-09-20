8c8b19c92efa6893345f1fedb877baba07cf535b
cem <cem@cem>
2016-09-20 15:35:18 +0300

    stable and dev versions splitted. 1.1 dev started.

--- pass.rb
+++ pass.rb
@@ -5,7 +5,7 @@ require_relative 'modules/call-list'
 require_relative 'modules/db-manager'
 require_relative 'modules/network'
 require_relative 'modules/parser'
-require_relative 'modules/scheduler'
+#require_relative 'modules/scheduler'
 require_relative 'modules/wallet'
 
 wallet = Wallet.new "wallet-record.db", "wallet-program.db"
@@ -14,11 +14,11 @@ scheduler_thread = Thread.new do
   Scheduler.run_service
 end
 =end
-network_thread = Thread.new do
+#network_thread = Thread.new do
   NetworkManager.run_server
-end
+#end
 
 #scheduler_thread.join
-network_thread.join
+#network_thread.join
 
 puts "fin."
