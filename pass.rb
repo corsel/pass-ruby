0eb841f33129ff6065805e6a2b68644b8957438b
cors <cors@cors-arch.localdomain>
2016-11-16 21:43:41 +0000

    stable

--- pass.rb
+++ pass.rb
@@ -1,11 +1,9 @@
 #! /usr/bin/ruby
 
 require_relative 'environment.rb'
-require_relative 'modules/call-list'
 require_relative 'modules/db-manager'
 require_relative 'modules/network'
 require_relative 'modules/parser'
-require_relative 'modules/scheduler'
 require_relative 'modules/wallet'
 
 wallet = Wallet.new "wallet-record.db", "wallet-program.db"
